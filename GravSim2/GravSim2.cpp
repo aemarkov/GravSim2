b160ee634b6c133b48a82f0975f9270f56853c04
Markov Alexey <markovalex95@gmail.com>
2016-08-17 20:30:37 +0300

    Добавил SDL

--- GravSim2/GravSim2.cpp
+++ GravSim2/GravSim2.cpp
@@ -2,10 +2,32 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <SDL2\SDL.h>
 
-
-int main()
+int main(int argc, char *argv[])
 {
+	//Init SDL
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+
+
+	//Create window
+	SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+	if (win == nullptr) {
+		std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+
+	//Create renderer
+	SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (ren == nullptr) {
+		std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+
     return 0;
 }
 
